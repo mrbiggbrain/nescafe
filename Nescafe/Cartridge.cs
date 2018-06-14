273805a2a4763fda5ed04c893b06e734f3a0416e
Nicholas Young <mrbiggbrain@gmail.com>
2018-06-14 19:16:08 -0400

    Error handeling code.

--- Nescafe/Cartridge.cs
+++ Nescafe/Cartridge.cs
@@ -185,63 +185,73 @@ namespace Nescafe
 
         void ParseHeader(BinaryReader reader)
         {
-            
-            // Verify magic number
-            uint magicNum = reader.ReadUInt32();
-            if (magicNum != HeaderMagic)
+            try
             {
-                System.Console.WriteLine("Magic header value (" + magicNum.ToString("X4") + ") is incorrect");
+                // Verify magic number
+                uint magicNum = reader.ReadUInt32();
+                if (magicNum != HeaderMagic)
+                {
+                    System.Console.WriteLine("Magic header value (" + magicNum.ToString("X4") + ") is incorrect");
+                    Invalid = true;
+                    return;
+                }
+
+                // Size of PRG ROM
+                PrgRomBanks = reader.ReadByte();
+                System.Console.WriteLine((16 * PrgRomBanks).ToString() + "Kb of PRG ROM");
+
+                // Size of CHR ROM (Or set CHR RAM if using it)
+                ChrBanks = reader.ReadByte();
+                if (ChrBanks == 0)
+                {
+                    System.Console.WriteLine("Cartridge uses CHR RAM");
+                    ChrBanks = 2;
+                    UsesChrRam = true;
+                }
+                else
+                {
+                    System.Console.WriteLine((8 * ChrBanks).ToString() + "Kb of CHR ROM");
+                    UsesChrRam = false;
+                }
+
+                // Flags 6
+                // 76543210
+                // ||||||||
+                // |||||||+-Mirroring: 0: horizontal(vertical arrangement)(CIRAM A10 = PPU A11)
+                // |||||||             1: vertical(horizontal arrangement)(CIRAM A10 = PPU A10)
+                // ||||||+--1: Cartridge contains battery - backed PRG RAM($6000 - 7FFF) or other persistent memory
+                // |||||+---1: 512 - byte trainer at $7000 -$71FF(stored before PRG data)
+                // ||||+----1: Ignore mirroring control or above mirroring bit; instead provide four - screen VRAM
+                // ++++---- - Lower nybble of mapper number
+                _flags6 = reader.ReadByte();
+                VerticalVramMirroring = (_flags6 & 0x01) != 0;
+                System.Console.WriteLine("VRAM mirroring type: " + (VerticalVramMirroring ? "vertical" : "horizontal"));
+
+                BatteryBackedMemory = (_flags6 & 0x02) != 0;
+                if (BatteryBackedMemory) System.Console.WriteLine("Cartridge contains battery backed persistent memory");
+
+                ContainsTrainer = (_flags6 & 0x04) != 0;
+                if (ContainsTrainer) System.Console.WriteLine("Cartridge contains a 512 byte trainer");
+
+                // Flags 7
+                // 76543210
+                // ||||||||
+                // |||||||+-VS Unisystem
+                // ||||||+--PlayChoice - 10(8KB of Hint Screen data stored after CHR data)
+                // ||||++---If equal to 2, flags 8 - 15 are in NES 2.0 format
+                // ++++---- - Upper nybble of mapper number
+                _flags7 = reader.ReadByte();
+
+                // Mapper Number
+                MapperNumber = _flags7 & 0xF0 | (_flags6 >> 4 & 0xF);
+            }
+            catch(System.IO.EndOfStreamException e)
+            {
+                System.Console.WriteLine("Tried to read a section of the ROM that didn't exist. ");
                 Invalid = true;
                 return;
             }
-
-            // Size of PRG ROM
-            PrgRomBanks = reader.ReadByte();
-            System.Console.WriteLine((16 * PrgRomBanks).ToString() + "Kb of PRG ROM");
-
-            // Size of CHR ROM (Or set CHR RAM if using it)
-            ChrBanks = reader.ReadByte();
-            if (ChrBanks == 0) {
-                System.Console.WriteLine("Cartridge uses CHR RAM");
-                ChrBanks = 2;
-                UsesChrRam = true;
-            }
-            else 
-            {
-                System.Console.WriteLine((8 * ChrBanks).ToString() + "Kb of CHR ROM");
-                UsesChrRam = false;
-            }
-
-            // Flags 6
-            // 76543210
-            // ||||||||
-            // |||||||+-Mirroring: 0: horizontal(vertical arrangement)(CIRAM A10 = PPU A11)
-            // |||||||             1: vertical(horizontal arrangement)(CIRAM A10 = PPU A10)
-            // ||||||+--1: Cartridge contains battery - backed PRG RAM($6000 - 7FFF) or other persistent memory
-            // |||||+---1: 512 - byte trainer at $7000 -$71FF(stored before PRG data)
-            // ||||+----1: Ignore mirroring control or above mirroring bit; instead provide four - screen VRAM
-            // ++++---- - Lower nybble of mapper number
-            _flags6 = reader.ReadByte();
-            VerticalVramMirroring = (_flags6 & 0x01) != 0;
-            System.Console.WriteLine("VRAM mirroring type: " + (VerticalVramMirroring ? "vertical" : "horizontal"));
-
-            BatteryBackedMemory = (_flags6 & 0x02) != 0;
-            if (BatteryBackedMemory) System.Console.WriteLine("Cartridge contains battery backed persistent memory");
-
-            ContainsTrainer = (_flags6 & 0x04) != 0;
-            if (ContainsTrainer) System.Console.WriteLine("Cartridge contains a 512 byte trainer");
-
-            // Flags 7
-            // 76543210
-            // ||||||||
-            // |||||||+-VS Unisystem
-            // ||||||+--PlayChoice - 10(8KB of Hint Screen data stored after CHR data)
-            // ||||++---If equal to 2, flags 8 - 15 are in NES 2.0 format
-            // ++++---- - Upper nybble of mapper number
-            _flags7 = reader.ReadByte();
-
-            // Mapper Number
-            MapperNumber = _flags7 & 0xF0 | (_flags6 >> 4 & 0xF);
+               
         }
 
         private BinaryReader LoadZipFile(String path)
